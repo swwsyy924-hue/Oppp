770efe5d6f8739d5f3dd3c3dccfe9d9628e614bd
swwsyy924-hue <swwsyy924@gmail.com>
2026-06-03 15:59:38 +0300

    Update real_bot.py

--- real_bot.py
+++ real_bot.py
@@ -2,11 +2,8 @@ import discord
 from config import *
 from control_panel import process_control_command
 
-# إعداد البوت الحقيقي
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot_client = discord.Client(intents=intents)
+# البوت الحقيقي (بدون Intents للتوافق مع الإصدار القديم)
+bot_client = discord.Client()
 
 @bot_client.event
 async def on_ready():
@@ -14,12 +11,8 @@ async def on_ready():
 
 @bot_client.event
 async def on_message(message):
-    # تجاهل رسائل البوت نفسه أو أي بوت آخر
     if message.author.bot:
         return
-
-    # استمع فقط لمالك البوت (OWNER_ID) في أي مكان (DM أو قناة)
     if message.author.id == OWNER_ID:
-        # استيراد السيلف-بوت من main.py لتمريره لدالة الأوامر
         from main import bot as self_bot
         await process_control_command(message, self_bot)
